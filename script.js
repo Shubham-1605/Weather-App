942a8967d49b9efd8cfd9bdaa7c2146c76c504bf
Shubham-1605 <shubham1605dj@gmail.com>
2024-12-25 21:06:48 +0530

    created weather app

---
+++ script.js
@@ -0,0 +1,51 @@
+const inputbox = document.querySelector('.inputbox');
+const searchbtn = document.getElementById('searchbtn');
+const weather_img = document.querySelector('.weather-img');
+const temperature = document.querySelector('.temperature');
+const description = document.querySelector('.description');
+const humidity = document.getElementById('humidity');
+const wind_speed = document.getElementById('wind-speed');
+const location_not_found = document.querySelector('.location-not-found');
+const weather_body = document.querySelector('.weather-body');
+
+
+async function checkweather(city){
+    const api_key = "2fb8efb5cb25b6513c6083236de0a085";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+    const weather_data = await fetch(`${url}`).then(Response => Response.json());
+
+    if(weather_data.cod === `404`){
+        location_not_found.style.display = "flex";
+        weather_body.style.display = "none";
+        return;
+    }
+
+    location_not_found.style.display = "none";
+    weather_body.style.display = "flex";
+    temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
+    description.innerHTML = `${weather_data.weather[0].description}`;
+    humidity.innerHTML = `${weather_data.main.humidity}%`;
+    wind_speed.innerHTML = `${weather_data.wind.speed}km/h`;
+
+    switch(weather_data.weather[0].main){
+        case `Clouds`:
+            weather_img.src = "/weather_photo/cloud.png";
+            break;
+        case `Clear`:
+            weather_img.src = "/weather_photo/clear.png";
+            break;
+        case `Mist`:
+            weather_img.src = "/weather_photo/mist.png";
+            break;
+        case `Rain`:
+            weather_img.src = "/weather_photo/rain.png";
+            break;
+        case `Snow`:
+            weather_img.src = "/weather_photo/snow.png";
+            break;
+    }
+}
+
+searchbtn.addEventListener('click',()=>{
+    checkweather(inputbox.value);
+});
